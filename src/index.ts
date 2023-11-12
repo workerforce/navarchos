e9e0ee18bf6a0d2bbb02d2fab698984312238ba2
Will Walker <w_walker@icloud.com>
2023-11-12 03:52:56 -0600

    store the blob

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { OpenAPIRouter } from '@cloudflare/itty-router-openapi'
 const router = OpenAPIRouter()
 
 const DUMMY_UUID = "a39d9414-857c-48ff-b4bb-26e9074e34a1"
-const ALPINE_SIZE = 3331831
 
 router.get('/v2/', () => {
   return new Response('{}', {
@@ -16,8 +15,9 @@ router.get('/v2/', () => {
 
 router.head('/v2/:name+/blobs/:digest', async (request, env) => {
   const { name, digest } = request.params;
-  const storedDigest = await env.NAVAR_KV.get(name);
-  const exists = (storedDigest === digest);
+  const storedName = await env.NAVAR_KV.get(digest);
+  // const exists = (storedName == name);
+  const exists = false;
   return new Response(null, {
     status: exists ? 200 : 404,
     headers: { 
@@ -43,23 +43,24 @@ router.patch('/v2/:name+/blobs/uploads/:uuid', async (request, env) => {
   const { name } = request.params;
   await env.NAVAR_BUCKET.put(`${name}/blobs/uploads/${DUMMY_UUID}`, request.body);
   return new Response(null, {
-    status: 201,
+    status: 202,
     headers: {
       'Location': `/v2/${name}/blobs/uploads/${DUMMY_UUID}`,
-      'Range': `0-${ALPINE_SIZE - 1}`,
+      'Range': `0-${3331831 - 1}`,
       'Content-Length': '0',
     }
   });
 });
 
 router.put('/v2/:name+/blobs/uploads/:uuid', async (request, env) => {
+  const { name } = request.params;
   const { digest } = request.query;
-  await env.NAVAR_KV.put(request.params.name, digest);
+  await env.NAVAR_KV.put(digest, name);
   return new Response(null, {
     status: 201,
     headers: {
-      'Location': `/v2/${request.params.name}/blobs/uploads/${DUMMY_UUID}`,
-      'Range': `0-${ALPINE_SIZE - 1}`,
+      'Location': `/v2/${name}/blobs/uploads/${DUMMY_UUID}`,
+      'Range': `0-${3331831 - 1}`,
       'Content-Length': '0',
       'Docker-Content-Digest': digest
     }
@@ -69,13 +70,11 @@ router.put('/v2/:name+/blobs/uploads/:uuid', async (request, env) => {
 router.put('/v2/:name+/manifests/:reference', async (request, env) => {
   const { name } = request.params;
   await env.NAVAR_BUCKET.put(`${name}/blobs/uploads/manifest.json`, request.body);
-  const storedDigest = await env.NAVAR_KV.get(name);
   return new Response(null, {
     status: 201,
     headers: {
       'Location': `${name}/blobs/uploads/manifest.json`,
-      'Content-Length': '0',
-      'Docker-Content-Digest': storedDigest
+      'Content-Length': '0'
     }
   });
 });
